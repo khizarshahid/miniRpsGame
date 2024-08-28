cc0d71b907d6c953567eb77fcde8409fbcd9b2b8
Khizar Shahid <khyzershahidghouri@gmail.com>
2024-08-28 20:07:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+/// "rock" ,"papers", "scissors" program
+
+const choices = ["rock" ,"paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+const playGame = (playerChoice) => {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result= "";
+    
+    if(computerChoice === playerChoice){
+        result = "IT'S A TIE";
+    }
+    else {
+        switch(playerChoice){
+            case "rock":
+            result =    (computerChoice === "scissors" ) ? "You WIN!" : "You LOSE";
+            break;
+            case "paper":
+            result =    (computerChoice === "rock" ) ? "You WIN!" : "You LOSE";
+            break;
+            case "scissors":
+            result =    (computerChoice === "paper" ) ? "You WIN!" : "You LOSE";
+            break;
+
+        }
+        
+        playerDisplay.textContent = `PLAYER➡ ${playerChoice}`;
+        computerDisplay.textContent = `COMPUTER➡ ${computerChoice}`;
+        resultDisplay.textContent = result;
+
+        resultDisplay.classList.remove("greenText", "redText");
+
+        switch(result){
+            case "You WIN!":
+                resultDisplay.classList.add("greenText");
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                break;
+            case "You LOSE":
+                resultDisplay.classList.add("redText");
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                break;
+        }
+
+    }
+
+}
